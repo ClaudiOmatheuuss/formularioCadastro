9b0c3d32b1065af0ca8db49d5ff566f48bcc03d2
Claud10matheus <claudiom.pst@gmail.com>
2023-09-12 12:46:23 -0300

    criando forms

---
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -0,0 +1,51 @@
+import { Button, TextField } from '@mui/material';
+import React from 'react';
+
+function DadosEntrega () {
+    return (
+        <form>
+             <TextField
+                id="cep" 
+                label="CEP" 
+                type="number"
+                variant="outlined"
+                margin="normal"
+            />
+            <TextField 
+                id="endereco"
+                label="Endereco"
+                type="text"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <TextField 
+                id="numero"
+                label="Numero"
+                type="number"
+                variant="outlined"
+                margin="normal"
+            />
+            <TextField 
+                id="estado"
+                label="Estado"
+                type="text"
+                variant="outlined"
+                margin="normal"
+            />
+            <TextField 
+                id="cidade"
+                label="Cidade"
+                type="text"
+                variant="outlined"
+                margin="normal"
+            />
+
+            <Button type="submit" variant="contained" color="primary" fullWidth >
+                Finalizar cadastro
+            </Button>
+        </form>
+    )
+}
+
+export default DadosEntrega
---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,31 @@
+import { Button, TextField } from "@mui/material";
+import React from "react";
+
+function DadosUsuario () {
+    return (
+        <form>
+            <TextField 
+                id="email" 
+                label="email" 
+                type="email"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <TextField 
+                id="senha"
+                label="senha"
+                type="password"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+
+            <Button type="submit" variant="contained" color="primary" >
+                Cadastrar
+            </Button>
+        </form>
+    )
+}
+
+export default DadosUsuario
