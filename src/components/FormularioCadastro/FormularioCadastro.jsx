0587846a655321d22659cd437d0d4355cccd584c
Claud10matheus <claudiom.pst@gmail.com>
2023-09-12 15:57:46 -0300

    substituindo o switch case

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,32 +3,23 @@ import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 import DadosEntrega from "./DadosEntrega";
 
-import { Typography } from "@mui/material";
-
 function FormularioCadastro({aoEnviar, validarCPF}) {
   
   const [etapaAtual, setEtapaAtual] = useState(0)
   
+  const formularios = [
+    <DadosUsuario aoEnviar={proximo} />,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />,
+    <DadosEntrega aoEnviar={aoEnviar} />
+  ]
+
   function proximo() {
     setEtapaAtual(etapaAtual + 1)
   }
 
-  function formularioAtual(etapaAtual) {
-    switch(etapaAtual){
-      case 0:
-        return <DadosUsuario aoEnviar={proximo} />
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF} />
-      case 2:
-        return <DadosEntrega aoEnviar={aoEnviar} />
-      default:
-        return <Typography>Erro ao ser renderizado</Typography>
-    }
-  
-  }
   return (
     <>
-      { formularioAtual(etapaAtual) }
+      { formularios[etapaAtual] }
     </>
   );
 }
