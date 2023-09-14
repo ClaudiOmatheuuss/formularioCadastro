d8baf27230c57446ae667e93a20ab6ed5377b85a
Claud10matheus <claudiom.pst@gmail.com>
2023-09-13 21:05:26 -0300

    melhorando experiencia do usuario

--- src/components/FormularioCadastro/DadosRecebidos.jsx
+++ src/components/FormularioCadastro/DadosRecebidos.jsx
@@ -1,9 +1,14 @@
 import { Typography } from "@mui/material";
 import React from "react";
 
+import imagemObrigado from './img/obrigado.png'
+
 function DadosRecebidos () {
     return (
-        <Typography variant="h4" align="center">Obrigado pelo cadastro!</Typography>
+        <>
+            <Typography variant="h5" align="center" fontFamily="cursive" color="#1976D2">Dados coletados. Obrigado pelo cadastro!</Typography>
+            <img src={imagemObrigado} width="70%" alt="Imagem de agradecimento do cadastro" />
+        </>
     )
 }
 
